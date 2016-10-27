82be51527d410662ad72af05c5ceacd77245ab7d
Marius Butuc <marius.butuc@gmail.com>
2016-10-27 00:28:16 -0400

    Elixir counts the number of arguments as part of the function name
      * http://elixir-lang.org/getting-started/modules.html#default-arguments

--- elixir/hello-world/hello_world.exs
+++ elixir/hello-world/hello_world.exs
@@ -1,9 +1,28 @@
 defmodule HelloWorld do
-  def hello do
-    "Hello, World!"
-  end
+  @moduledoc """
+  Elixir counts the number of arguments as part of the function name.
+  For instance;
 
-  def hello(name) do
+      def hello() do
+      end
+
+  would be a completely different function from
+
+      def hello(name) do
+      end
+
+  Can you find a way to make all the tests pass with just one
+  function?
+
+  Hint: look into argument defaults here:
+  http://elixir-lang.org/getting-started/modules.html#default-arguments
+  """
+
+  @doc """
+  Greets the user by name, or by saying "Hello, World!"
+  if no name is given.
+  """
+  def hello(name \\ "World") do
     "Hello, #{name}!"
   end
 end
